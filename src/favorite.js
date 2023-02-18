d8a34e713560e3e8083ff86219bd7af76599cfb4
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2023-02-18 11:54:49 +0200

    +sprite

---
+++ src/favorite.js
@@ -0,0 +1,12 @@
+
+button.addEventListener('click', removeFromLocalStorage);
+
+function removeFromLocalStorage() {
+    localStorage.removeItem("");
+};
+
+function addCurentStylePage() {
+    elem.classList.add(header__page-curent)
+};
+addCurentStylePage()
+
