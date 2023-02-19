b91e09702712d3a98f609c3dfbc9268b0adb7713
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2023-02-19 18:10:42 +0200

    1

--- src/favorite.js
+++ src/favorite.js
@@ -1,8 +1,35 @@
+import createCards from "./js/markup/index";
 
-button.addEventListener('click', removeFromLocalStorage);
+const addToFavouriteBtn = querySelector(".item-news__add-to-favorite");
+const containerCard = querySelector(".container-card");
+let toFavorite = [];
+if (toFavorite = true) {
 
-function removeFromLocalStorage() {
-    localStorage.removeItem("");
+}
+
+localStorage.setItem("toFavorite", JSON.stringify(createCards));
+
+addToFavouriteBtn.addEventListener('click', addToLocalStorage);
+
+function addToLocalStorage(createCards) {
+    toFavorite = true;
+    
+    insertCard();
+};
+function insertCard(createCards) {     
+    containerCard.insertAdjacentHTML("beforeend", createCards);     
+};
+
+addToFavouriteBtn.addEventListener('click', changeBtn);
+function changeBtn() {
+    addToFavouriteBtn.textContent = "Remove from favorite";
+    addToFavouriteBtn.svg.use.href = "./images/symbol-defs.svg#icon-removefavorite";  
+};
+
+addToFavouriteBtn.addEventListener('click', removeFromLocalStorage);
+
+function removeFromLocalStorage(createCards) {
+    localStorage.removeItem("createCards");
 };
 
 function addCurentStylePage() {
