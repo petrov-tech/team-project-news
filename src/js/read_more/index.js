26952dd8122d3cf7d0a1e37d895a2ec183aa6661
Yuriichuk-Olha <olga.kantemyr@gmail.com>
2023-02-20 15:12:20 +0100

    add

---
+++ src/js/read_more/index.js
@@ -0,0 +1,9 @@
+import massPageCards from "./js/markup/index.js";
+import loadCardsHtml from "./js/markup/index.js";
+import creatCards from "./js/markup/index.js";
+//=== через data-id="${idCards}" порівняти з let massPageCards = []
+
+
+const listCard = document.querySelector('.list__card');
+const dataList = document.querySelector('.date-list__data');
+const dataIcon = document.querySelector('.date-list__icon');
