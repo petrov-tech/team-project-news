c5010cdf3085a8fe7306a434b7e1f8596983026e
Aleksandr Borysenko <borysenko343@gmail.com>
2023-02-20 23:15:42 +0200

    delete fill of svfg, datapicker size for mobile

--- src/js/calendar/index.js
+++ src/js/calendar/index.js
@@ -2,3 +2,8 @@ import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 
 new AirDatepicker('.calendar_input');
+
+const datepicker = document.querySelector('#input-picker');
+datepicker.addEventListener('show', function () {
+  datepicker.classList.add('switched');
+});
