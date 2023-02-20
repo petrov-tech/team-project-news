b1779251a6f71ae4eb298f868d04c854c4f9e5e1
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2023-02-20 22:19:42 +0200

    favorite

--- src/favorite.js
+++ src/favorite.js
@@ -1,8 +1,12 @@
-import createCards from "./js/markup/index";
+// import createCards from "./js/markup/index.js";
+// import massPageCards from "./js/markup/index.js";
 
 const container = querySelector(".new");
 const containerCard = querySelector(".container-card");
-const toFavouriteBtn = querySelector(".item-news__add-to-favorite");
+// const toFavouriteBtn = querySelector(".item-news__add-to-favorite");
+const navList = document.querySelector('.navigation_list');
+// const favoriteNavLink = document.querySelector('.navigation_link')
+
 
 let data = [];
 
@@ -14,7 +18,7 @@ function addTofavorite(e) {
     if (e.target.nodeName !== "BUTTON") {
     return;
     }    
-    const curentBtn = e.target.toFavouriteBtn;   
+    const curentBtn = e.target;   
     curentBtn.classList.replace(".item-news__add-to-favorite-btn", ".item-news__add-to-favorite-btn-remove");    
     data.push(idCards);    
 };
@@ -24,13 +28,21 @@ function removeFromFavorite() {
      if (e.target.nodeName !== "BUTTON") {
     return;
     };
-    const curentBtn = e.target.toFavouriteBtn;
+    const curentBtn = e.target;
     curentBtn.classList.replace(".item-news__add-to-favorite-btn-remove", ".item-news__add-to-favorite-btn" )
     data.splice(idCards.index, 1);    
 };
 
+navList.addEventListener('click', addCurentStylePage);
+
 function addCurentStylePage() {
-    elem.classList.add(header__page-curent)
+     if (e.target.nodeName !== "A") {
+    return;
+    };
+    const favoriteNav = e.target;
+    const currentActiveLink = document.querySelector('.navigation__link - curent')
+    favoriteNav.classList.add(navigation__link - curent);
+    currentActiveLink.classList.remove('.navigation__link - curent')
 };
-addCurentStylePage()
+addCurentStylePage();
 
