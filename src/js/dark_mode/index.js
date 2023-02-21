bd02291ae267e7360307312404d5a06a6dff0476
Vastasika <kykysia13@gmail.com>
2023-02-21 20:01:11 +0200

    dark mode

---
+++ src/js/dark_mode/index.js
@@ -0,0 +1,40 @@
+const toggles = document.querySelectorAll('.switch_checkbox');  //откорректировать класс 
+
+let themeKey = localStorage.getItem("themeKey")|| 'light';
+
+const body = document.querySelector('body');
+
+
+
+
+function colorTheme(e){
+    if (e.type==='click')  {
+        themeKey = themeKey==='light'?'dark':'light';
+
+     localStorage.setItem("themeKey", themeKey);
+
+    }   
+
+
+    body.classList.toggle('light__theme');
+    body.classList.toggle('dark__theme');//body
+
+
+
+
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", (e)=> {
+    if (themeKey === 'dark') {
+        colorTheme(e);
+    }
+});
+
+toggles.forEach(toggle=> {toggle.addEventListener('click',colorTheme)
+  if(themeKey === 'dark'){
+    toggle.checked=true;}
+    else{toggle.checked=false;}
+});
