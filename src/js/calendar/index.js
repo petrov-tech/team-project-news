924513847f7fe814f56ffa96fb409b443e882221
Aleksandr Borysenko <borysenko343@gmail.com>
2023-02-21 21:24:28 +0200

    add css

--- src/js/calendar/index.js
+++ src/js/calendar/index.js
@@ -1,9 +1,19 @@
 import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 
-new AirDatepicker('.calendar_input');
-
 const datepicker = document.querySelector('#input-picker');
-datepicker.addEventListener('show', function () {
-  datepicker.classList.add('switched');
+const calendar = document.querySelector('.calendar__button-calendar');
+const arrow = document.querySelector('.calendar__button-arrow');
+
+new AirDatepicker('.calendar_input', {
+  onShow() {
+    datepicker.classList.add('isActive');
+    calendar.classList.add('switchedColor');
+    arrow.classList.add('switched');
+  },
+  onHide() {
+    datepicker.classList.remove('isActive');
+    calendar.classList.remove('switchedColor');
+    arrow.classList.remove('switched');
+  },
 });
