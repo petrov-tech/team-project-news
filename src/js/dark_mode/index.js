1187a831b0d06ac927f9c549f2872eea2286260a
Vastasika <kykysia13@gmail.com>
2023-02-22 01:29:52 +0200

    add css

--- src/js/dark_mode/index.js
+++ src/js/dark_mode/index.js
@@ -3,9 +3,6 @@ let themeKey = localStorage.getItem("themeKey")|| 'light';
 
 const body = document.querySelector('body');
 
-
-
-
 function colorTheme(e){
     if (e.type==='click')  {
         themeKey = themeKey==='light'?'dark':'light';
@@ -13,18 +10,8 @@ function colorTheme(e){
      localStorage.setItem("themeKey", themeKey);
 
     }   
-
-
-    body.classList.toggle('light__theme');
-    body.classList.toggle('dark__theme');//body
-
-
-
-
-
+    body.classList.toggle('dark__theme');
 }
-
-
 
 document.addEventListener("DOMContentLoaded", (e)=> {
     if (themeKey === 'dark') {
