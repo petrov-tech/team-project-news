d1f3f717c65343bfcf6706c366b24328677e3212
petrov-tech <110338544+petrov-tech@users.noreply.github.com>
2023-02-22 01:10:15 +0000

    Deploying to gh-pages from @ petrov-tech/team-project-news@586381172a1eaf70479055cb8fd490d4f9488957 🚀

--- read.7bebaa08.js
+++
@@ -1,2 +0,0 @@
-!function(){var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},r={},i=e.parcelRequired7c6;null==i&&((i=function(e){if(e in n)return n[e].exports;if(e in r){var i=r[e];delete r[e];var o={id:e,exports:{}};return n[e]=o,i.call(o.exports,o,o.exports),o.exports}var l=new Error("Cannot find module '"+e+"'");throw l.code="MODULE_NOT_FOUND",l}).register=function(e,n){r[e]=n},e.parcelRequired7c6=i),i("iryiF"),i("458gi")}();
-//# sourceMappingURL=read.7bebaa08.js.map
